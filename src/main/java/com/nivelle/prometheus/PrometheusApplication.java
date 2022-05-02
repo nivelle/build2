0c34c18cae0b503ddeace683d1c8a4d04625ceab
fuxinzhong <fuxinzhong@zhangyue.com>
2022-05-02 14:43:55 +0800

    【增加heart】

--- src/main/java/com/nivelle/prometheus/PrometheusApplication.java
+++ src/main/java/com/nivelle/prometheus/PrometheusApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PrometheusApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(PrometheusApplication.class, args);
     }
 
     @Bean
