0aac432946faa68a4beb84f2fb57063630c9b78b
fuxinzhong <fuxinzhong@zhangyue.com>
2022-05-02 15:09:53 +0800

    【增加heart】

--- src/main/java/com/nivelle/prometheus/controller/HeartController.java
+++ src/main/java/com/nivelle/prometheus/controller/HeartController.java
@@ -2,7 +2,7 @@ package com.nivelle.prometheus.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * TODO:DOCUMENT ME!
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author fuxinzhong
  * @date 2022/05/02
  */
-@RestController
+@RequestMapping("/test")
 public class HeartController {
 
     @RequestMapping(value = "/heart",method = RequestMethod.GET)
+    @ResponseBody
     public String heart(String a) {
         return "hello heart:" + a;
     }
