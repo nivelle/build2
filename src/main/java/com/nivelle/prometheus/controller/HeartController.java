c1e2f113b1b11a75779cf203d0798fcc304abd18
fuxinzhong <fuxinzhong@zhangyue.com>
2022-06-18 11:33:40 +0800

    【增加prometheus】

--- src/main/java/com/nivelle/prometheus/controller/HeartController.java
+++ src/main/java/com/nivelle/prometheus/controller/HeartController.java
@@ -1,5 +1,6 @@
 package com.nivelle.prometheus.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
