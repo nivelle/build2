b061f53a0055e2b0b611640bc7bef873b1834389
fuxinzhong <fuxinzhong@zhangyue.com>
2022-06-18 12:23:16 +0800

    【增加prometheus】

--- src/main/java/com/nivelle/prometheus/PrometheusApplication.java
+++ src/main/java/com/nivelle/prometheus/PrometheusApplication.java
@@ -1,7 +1,11 @@
 package com.nivelle.prometheus;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrometheusApplication {
@@ -9,6 +13,12 @@ public class PrometheusApplication {
     public static void main(String[] args) {
         SpringApplication.run(PrometheusApplication.class, args);
     }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+
 
 
 }
