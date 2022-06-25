245c21f0711c946d8a918a81b2af9a3f848f249f
fuxinzhong <fuxinzhong@zhangyue.com>
2022-06-25 16:41:51 +0800

    【增加schedule】

--- src/main/java/com/nivelle/prometheus/ScheduleJob.java
+++ src/main/java/com/nivelle/prometheus/ScheduleJob.java
@@ -1,10 +1,14 @@
 package com.nivelle.prometheus;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * TODO:DOCUMENT ME!
@@ -15,6 +19,7 @@ import java.util.List;
 @Component
 public class ScheduleJob {
     public int a = 0;
+
 
     @Scheduled(cron = "0 0/5 * * * ?")
     public void myJob() {
@@ -29,5 +34,37 @@ public class ScheduleJob {
             System.out.println("清空一次集合");
         }
         System.out.println("end =====");
+        ReentrantLock lock = new ReentrantLock(true);
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 30; i++) {
+            try {
+                pool.execute(new Task(String.valueOf(i),lock));
+            }catch (Exception e){
+
+            }
+        }
+
+
     }
+}
+
+class Task implements Runnable {
+    private String name;
+    private ReentrantLock lock;
+
+    public Task(String name, ReentrantLock lock) {
+        this.name = name;
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        lock.lock();
+        try {
+            System.out.println(this.name + "---运行的线程" + Thread.currentThread().getName());
+        } finally {
+            lock.unlock();
+        }
+    }
+
 }
